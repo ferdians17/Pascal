db0b0b19fdbc5a8b496ca02f0361a166bbcac3ca
ferdians17 <penyalahgunaanakun@gmail.com>
2024-11-08 10:55:06 +0700

    Tugas 1

---
+++ Tugas_Mingguan/Tugas_1.pas
@@ -0,0 +1,17 @@
+program Perbandingan_Fittra_Ferdiansyah;
+uses crt;
+var a,b : integer;
+begin
+    clrscr;
+    a := 6; b := 8;
+
+    writeln('a = ', a);
+    writeln('b = ', b);
+    writeln('a =  b', ' :', (a=b));
+    writeln('a <> b', ' :', (a <> b));
+    writeln('a >  b', ' :', (a > b));
+    writeln('a <  b', ' :', (a < b));
+    writeln('a >= b', ' :', (a >= b));
+    writeln('a <= b', ' :', (a <= b));
+    readln;
+end.
