7b1b9543aa0c47acae6d6b43c3936a37bff57654
ferdians17 <penyalahgunaanakun@gmail.com>
2024-11-08 10:37:13 +0700

    Pertemuan 10

--- Pertemuan_10/10.2_Perulangan_For_Multi_Statement.pas
+++ Pertemuan_10/10.2_Perulangan_For_Multi_Statement.pas
@@ -3,33 +3,12 @@ uses crt;
 var awal, akhir:integer;
 
 begin    
-    // for akhir := 9 downto 1 do
-    //     begin
-    //         writeln(akhir);
-    //     end;
-    // read;
-
-    // for akhir := 17 downto 1 do
-    //     begin
-    //         awal := akhir + 2;
-    //         writeln(awal);
-    //     end;
-    // readln;
-    // clrscr;
-    // for awal := 1 to 5 do
-    // begin
-    //     for akhir := 1 to awal do
-    //     begin
-    //         gotoxy(3*awal, akhir); write(awal);
-    //     end;
-    // end;
     clrscr;
-    for awal := 5 downto 1 do
+    for awal := 1 to 5 do
     begin
-        for akhir := 5 to awal do
+        for akhir := 1 to awal do
         begin
-            gotoxy(awal - (akhir-1), akhir); write(awal);
+            gotoxy(3*awal, akhir); write(awal);
         end;
     end;
-
 end.
