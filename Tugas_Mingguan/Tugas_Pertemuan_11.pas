9e5cf1b68e8fd5e29be1f0de8d2563bcbfe616c9
ferdians17 <penyalahgunaanakun@gmail.com>
2024-11-16 21:17:59 +0700

    segitiga terbalik dengan for (lupis)

---
+++ Tugas_Mingguan/Tugas_Pertemuan_11.pas
@@ -0,0 +1,27 @@
+program repeat_until;
+uses crt;
+var p,i,j,k : integer;
+
+begin
+    clrscr;
+    writeln('########################');
+    writeln('#[ FITTRA FERDIANSYAH ]#');
+    writeln('########################');
+    write('masukkan panjang persegi :');
+    readln(p);
+
+    for i:= p downto 1 do
+    begin
+        for j:= i to p do
+        begin
+           write(' ');
+        end;
+
+        for k:= 1  to i  do
+        begin
+            write(' *');
+        end;
+        writeln;
+    end;
+    readln;
+end.
